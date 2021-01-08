02f10850514129b9c7ee70360f62c66d00248101
jack battle <jackonno78@hotmail.com>
2021-01-08 17:55:13 -0600

    added html elements,questions.js, and script.js

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+      title: "What is the full name of the band?",
+      multipleChoice: ["King Lizard Gizzard Wizard", "King Wizard and the Lizard Gizzard", "King Gizzard and the Lizard Wizard", "King Gizzard and the Wizard Lizard"],
+      answer: "King Gizzard and the Lizard Wizard"
+    },
+  
+    {
+      title: "The band is known for having 2 members play which instrument?",
+      multipleChoice: ["Drums", "Harmonica", "Guitar", "Keyboard/Synthesizer"],
+      answer: "Drums"
+    },
+  
+    {
+      title: "Stu Mackenzie plays a custom microtonal guitar named what?",
+      multipleChoice: ["Floating Macro Apple", "Microtonal Monstrosity", "Flying Microtonal Banana", "Mark"],
+      answer: "Flying Microtonal Banana"
+    },
+  
+    {
+      title: "What song on the album 'K.G' is a turkish house jam?",
+      multipleChoice: ["Intrasport", "Oddlife", "Minimum Brain Size", "Honey"],
+      answer: "Intrasport"
+    },
+  
+    {
+      title: "What is my favorite KGATLW record?",
+      multipleChoice: ["Polygondwanaland", "Nonagon Infinity", "Sketches Of Brunswick East", "Fishing For Fishies"],
+      answer: "Polygondwanaland"
+    }
+  ];
